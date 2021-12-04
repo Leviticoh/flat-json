8b2d868b6eda475e764ec718114c9f56e7781377
Leviticoh <epsilon.zeta@protonmail.com>
2021-12-04 08:19:19 +0100

    added json_trim_space routine

--- json.c
+++ json.c
@@ -1,9 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<errno.h>
 #include<stdint.h>
 #include<stdbool.h>
 #include<string.h>
+#include<limits.h>
+
+#define BUFSIZE 4096
+
+#define NOFILE -1
+
+
+#define EERRNO INT_MIN
+#define EHALT  0
+#define ENCOMP 1
+#define ESYNT  2
+
+
+extern int errno;
+int FJ_errno;
 
 typedef struct {
 	uint_fast8_t type;
@@ -30,6 +46,57 @@ typedef struct {
 	json_value value;
 	json_array* next;
 } json_array;
+
+
+/*buffer management functions*/
+
+ssize_t eob_callback(char* buffer, ssize_t* offset, int fd)
+{
+	ssize_t ret;
+	if(fd == NOFILE){
+		FJ_errno = ENCOMP;
+		return -1;
+	}
+
+	ret = read(fd, buffer, BUFSIZE);
+	if(ret == 0){
+		FJ_errno = ENCOMP;
+		return -1;
+	}
+
+	FJ_errno = EERRNO;
+
+	if(ret > -1 && ret < BUFSIZE){
+		buffer[ret] = '\0';
+	}
+
+	*offset = 0;
+	return ret;
+}
+
+/*trimming functions*/
+
+ssize_t json_trim_whitespace(char* buffer, ssize_t* offset, int fd)
+{
+	ssize_t start = *offset;
+	while(
+		buffer[*offset] == ' ' ||
+		buffer[*offset] == '\t' ||
+		buffer[*offset] == '\n' ||
+		buffer[*offset] == '\r'
+		){
+
+		(*offset) += 1;
+		if((*offset) == BUFSIZE || buffer[*offset] == '\0'){
+			if(eob_callback(buffer, offset, fd) == -1){
+				return -1;
+			}
+		}
+	}
+	return (*offset)-start;
+}
+
+/*parsing functions*/
 
 json_value json_parse_value(char* buffer, ssize_t* offset, int fd)
 {
